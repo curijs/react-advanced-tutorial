d21d3e23c0c2b4a68446328d4b42217f1049079d
Paul Sherman <paul.sherman.88@gmail.com>
2019-05-07 14:52:45 -0500

    Update Curi & Hickory

--- src/routes.js
+++ src/routes.js
@@ -1,59 +1,57 @@
 import { prepareRoutes } from "@curi/router";
 import { preferDefault } from "@curi/helpers";
 
-export default prepareRoutes({
-  routes: [
-    {
-      name: "Home",
-      path: "",
-      resolve(_, external) {
-        const body = import("./components/Home").then(preferDefault);
-        const books = external.bookAPI.BOOKS();
-        return Promise.all([ body, books ]);
-      },
-      respond({ resolved }) {
-        const [body, books] = resolved;
-        return {
-          body,
-          data: { books }
-        };
-      }
+export default prepareRoutes([
+  {
+    name: "Home",
+    path: "",
+    resolve(_, external) {
+      const body = import("./components/Home").then(preferDefault);
+      const books = external.bookAPI.BOOKS();
+      return Promise.all([ body, books ]);
     },
-    {
-      name: "Book",
-      path: "book/:id",
-      resolve({ params }, external) {
-        const body = import("./components/Book").then(preferDefault);
-        const book = external.bookAPI.BOOK(params.id);
-        return Promise.all([ body, book ]);
-      },
-      respond({ resolved }) {
-        const [body, book] = resolved;
-        return {
-          body,
-          data: { book }
-        };
-      }
-    },
-    {
-      name: "Checkout",
-      path: "checkout",
-      resolve() {
-        return import("./components/Checkout").then(preferDefault);
-      },
-      respond({ resolved }) {
-        return { body: resolved };
-      }
-    },
-    {
-      name: "Catch All",
-      path: "(.*)",
-      resolve() {
-        return import("./components/NotFound").then(preferDefault);
-      },
-      respond({ resolved }) {
-        return { body: resolved };
-      }
+    respond({ resolved }) {
+      const [body, books] = resolved;
+      return {
+        body,
+        data: { books }
+      };
     }
-  ]
-});
+  },
+  {
+    name: "Book",
+    path: "book/:id",
+    resolve({ params }, external) {
+      const body = import("./components/Book").then(preferDefault);
+      const book = external.bookAPI.BOOK(params.id);
+      return Promise.all([ body, book ]);
+    },
+    respond({ resolved }) {
+      const [body, book] = resolved;
+      return {
+        body,
+        data: { book }
+      };
+    }
+  },
+  {
+    name: "Checkout",
+    path: "checkout",
+    resolve() {
+      return import("./components/Checkout").then(preferDefault);
+    },
+    respond({ resolved }) {
+      return { body: resolved };
+    }
+  },
+  {
+    name: "Catch All",
+    path: "(.*)",
+    resolve() {
+      return import("./components/NotFound").then(preferDefault);
+    },
+    respond({ resolved }) {
+      return { body: resolved };
+    }
+  }
+]);
